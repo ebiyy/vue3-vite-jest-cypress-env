a88e255189becede8fd559aaa5b6c2e1f0123487
ebiyy <kuraim22@gmail.com>
2021-05-05 10:32:52 +0900

    first commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  moduleFileExtensions: ["js", "ts", "json", "vue", "tsx"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
